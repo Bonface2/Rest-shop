5687d2c8dcb653784677a48c443a96af88fb2a79
Bonface2 <bonnym2031@gmail.com>
2021-02-22 18:57:58 +0300

    Added body parser to parse request bodies, handled CORS errors

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,6 +1,7 @@
 const express = require ('express');
 const router = express.Router();
 
+
 router.get('/', (req, res, next)=> {
     res.status(200).json({
         message: "Orders were fetched"
@@ -8,10 +9,17 @@ router.get('/', (req, res, next)=> {
 });
 
 router.post('/', (req, res, next)=>{
+    const order={
+        productId: req.body.productId,
+        quantity: req.body.quantity
+    };
      res.status(201).json({
-        message: "Order was added"
+        message: "Order was added",
+        order:order
      });
 });
+
+
 
 router.get('/:orderId', (req, res, next)=>{
     res.status(200).json({
--- app.js
+++ app.js
@@ -1,14 +1,30 @@
 const express = require ('express'); //import express
 const app = express(); //executes express
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json()); 
 
 app.use('/products', productRoutes); //Incoming request must go through app.use
 app.use('/orders', orderRoutes);
+
+app.use((req, res, next)=>{
+    res.header("Access-Control-Allow-Origin","*");
+    res.header(
+    "Access-Control-Allow-Headers", 
+    "Origin, X_Requested-With, Content-Type, Accept, Authorization"
+    );
+    if(req.method==='OPTIONS'){
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
 
 app.use((req, res, next)=>{
     const error = new Error('Not found');
